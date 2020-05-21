389c8fcd22b73b8a7b39848cedd5dfc0b11d9a86
Zainabalhaidary <z.haidary@ymail.com>
2020-05-21 07:57:39 +0100

    Added Test case

--- src/Test/com/zainab/learning/ReassembleTextFragmentsTest.java
+++ src/Test/com/zainab/learning/ReassembleTextFragmentsTest.java
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.Test;
 
 class ReassembleTextFragmentsTest {
 
+	/*
+	 * 
+	 * Imagine you hav;page of text. You value this text and have no hard or soft copies of it;;ou have 5 copies of the same page of te
+	 * 
+	 * */
 	@Test
 	void testReassemble() {
-		String example1 = "O draconia;conian devil! Oh la;h lame sa;saint!";
-		String mergedLine = ReassembleTextFragments.reassemble(example1);
-		assertEquals("O draconian devil! Oh lame saint!", mergedLine);
+		String example1 = "Imagine you hav;page of text. You value this text and have no hard or soft copies of it;;ou have 5 copies of the same page of te";
+		String result = ReassembleTextFragments.reassemble(example1);
+		assertEquals("Imagine you have 5 copies of the same page of text. You value this text and have no hard or soft copies of it", result);
 	}
 
 }
